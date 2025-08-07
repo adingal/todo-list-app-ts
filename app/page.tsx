a6f231f4e40363a53b9d303ba6c96415f1c8bb39
Lhex <xelalagnid@gmail.com>
2025-08-08 01:15:05 +0800

    Add header and form components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import Header from '@/components/Header';
+import Form from '@/components/Form';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-4 md:py-5 lg:py-6'>
+        <div className='max-w-md mx-auto px-4 text-center'>
+          <Form />
+        </div>
+      </main>
+    </>
   );
 }
